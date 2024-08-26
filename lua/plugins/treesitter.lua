c1ce3042196af8d9bac6e08fbb9a82aa1cf74bd1
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2024-08-26 08:21:50 -0300

    change treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,9 @@ return {
       config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-          ensure_installed = {"lua", "javascript"},
+          -- ensure_installed = {"lua", "javascript", "bash", "c" },
+          -- sync_install = false,
+          auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
         })
