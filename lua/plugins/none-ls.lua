2f9a331d9e0288cedf0e486105e50caed2e50db2
Vilmar Bispo Filho <vilmar.bispo-ext@ab-inbev.com>
2024-08-26 21:45:27 -0300

    fixed load of eslint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,17 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+        require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 			},
 		})
 
