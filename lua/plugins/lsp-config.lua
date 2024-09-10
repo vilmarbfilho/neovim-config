67f57e07ef10e2e0077dee03d071db1ead41a1a9
Vilmar Bispo Filho <vilmar.bispo-ext@ab-inbev.com>
2024-09-10 20:42:08 -0300

    update plugins and change setup of lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
+        ensure_installed = { "lua_ls", "ts_ls" }
       })
     end
   },
@@ -17,8 +17,16 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({})
+          end
+        },
+      })
+      -- lspconfig.lua_ls.setup({})
+      -- lspconfig.tsserver.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
